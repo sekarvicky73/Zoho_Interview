88660090385088afc2f675f4353fc7dbe1f52249
Sekar <61583259+sekarvicky73@users.noreply.github.com>
2021-07-30 08:03:48 +0530

    Update 3.c

--- Set 2/3.c
+++ Set 2/3.c
@@ -28,3 +28,26 @@ int main()
  }
 return 0;
 }
+
+
+
+
+
+#2
+
+
+
+#include<stdio.h>
+int main()
+{
+     int n,k=1,a[10000],i;
+     a[0]=3;a[1]=4;
+     scanf("%d",&n);
+    for(i=0;i<n;i++)
+   {
+    a[++k]=(a[i]*10)+3;
+    a[++k]=(a[i]*10)+4;
+    }
+   for(i=0;i<n;i++)
+   printf("%d ",a[i]);
+}
